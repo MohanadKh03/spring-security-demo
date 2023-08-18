4fc6a82ddb1fff5eae3e3d48cd903a4b6f958a15
Mohanad <mohanadkhaled87@gmail.com>
2023-08-18 17:13:50 +0300

    added basic auth

---
+++ basic-auth/src/main/java/com/example/basicauth/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.basicauth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    String hello(){
+        return "Hello world";
+    }
+    @GetMapping("test")
+    String test(){
+        return "test";
+    }
+}
